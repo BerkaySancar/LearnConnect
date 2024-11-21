9745d8d69a46bc89537140c75e9ac81861c14f99
Berkay Sancar <sancarberkay@gmail.com>
2024-11-21 17:38:05 +0300

    Initial commit

---
+++ LearnConnect/LearnConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnConnectApp.swift
+//  LearnConnect
+//
+//  Created by Berkay Sancar on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
