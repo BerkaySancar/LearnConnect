95a8415fe636969a1cf1013c19690405f2114f2b
Berkay Sancar <sancarberkay@gmail.com>
2024-11-23 22:12:17 +0300

    complete FavoritesView without data connectivity

--- LearnConnect/Core/Favorites/FavoritesView.swift
+++ LearnConnect/Core/Favorites/FavoritesView.swift
@@ -11,13 +11,73 @@ struct FavoritesView: View {
     
     @StateObject private var viewModel = FavoritesViewModel()
     
+    @State private var isPresentedDeleteAllConfirm = false
+    
+    var courses: [Course] = [
+//                  Course(title: "SwiftUI Essentials", imageName: "swift"),
+//                  Course(title: "Mastering Combine", imageName: "combine"),
+//                  Course(title: "iOS Animations", imageName: "animation"),
+//                  Course(title: "CoreData Deep Dive", imageName: "coredata")
+    ]
+    
     var body: some View {
-        VStack {
-            DefaultTopView(title: "Favorites")
-            
-            Spacer()
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                ZStack {
+                    DefaultTopView(title: "Favorites")
+                    HStack {
+                        Spacer()
+                        if !courses.isEmpty {
+                            Button {
+                                isPresentedDeleteAllConfirm.toggle()
+                            } label: {
+                                Image(systemName: "trash")
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                            .confirmationDialog("Are you sure want to delete all favorites?", isPresented: $isPresentedDeleteAllConfirm, titleVisibility: .visible) {
+                                Button("Delete All", role: .destructive) {
+                                    
+                                }
+                            }
+                        }
+                    }
+                    .padding(.trailing, 26)
+                }
+                
+                if courses.isEmpty {
+                    EmptyContentView(
+                        systemImage: "heart.slash.fill",
+                        title: "No Favorites Yet",
+                        description: "Find courses you love and add them to your favorites!"
+                    )
+                    .padding()
+                } else {
+                    ContentView(geometry: geometry)
+                }
+       
+                Spacer()
+            }
+            .background(.appBackground)
         }
     }
+}
+
+// MARK: - UI Elements Extension
+extension FavoritesView {
+    @ViewBuilder
+    private func ContentView(geometry: GeometryProxy) -> some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(courses) { course in
+                    CourseCardView(course: course)
+                        .frame(width: geometry.size.width - 32, height: 230)
+                }
+            }
+        }
+        .padding(.top, 22)
+    }
+    
 }
 
 #Preview {
