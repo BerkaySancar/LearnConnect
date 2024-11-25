617192eb474dfddc930db95bd874737acaf8b22a
Berkay Sancar <sancarberkay@gmail.com>
2024-11-25 15:03:54 +0300

    adds EnrolledCourses table and service

---
+++ LearnConnect/Database/EnrolledCourses+CoreDataClass.swift
@@ -0,0 +1,24 @@
+//
+//  EnrolledCourses+CoreDataClass.swift
+//  LearnConnect
+//
+//  Created by Berkay Sancar on 25.11.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(EnrolledCourses)
+public class EnrolledCourses: NSManagedObject, Identifiable {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<EnrolledCourses> {
+        return NSFetchRequest<EnrolledCourses>(entityName: "EnrolledCourses")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var userId: String?
+    @NSManaged public var courseId: String?
+    @NSManaged public var courseName: String?
+    @NSManaged public var thumbnail: String?
+}
---
+++ LearnConnect/Services/EnrolledCoursesService.swift
@@ -0,0 +1,71 @@
+//
+//  EnrolledCoursesService.swift
+//  LearnConnect
+//
+//  Created by Berkay Sancar on 25.11.2024.
+//
+
+import CoreData
+import Foundation
+
+final class EnrolledCoursesService {
+    
+    static let shared = EnrolledCoursesService()
+    
+    private let context: NSManagedObjectContext
+    private let currentUserService: CurrentUserService
+    
+    private init(context: NSManagedObjectContext = DatabaseManager.shared.context, currentUserService: CurrentUserService = .shared) {
+        self.context = context
+        self.currentUserService = currentUserService
+    }
+    
+    func addCourseToEnrolledCourses(course: Course) {
+        let enrolledCourse = EnrolledCourses(context: context)
+        enrolledCourse.id = UUID().uuidString
+        enrolledCourse.courseId = course.id
+        enrolledCourse.userId = currentUserService.getCurrentUser()?.id
+        enrolledCourse.thumbnail = course.thumbnail
+        enrolledCourse.courseName = course.name
+        save()
+    }
+    
+    func getEnrolledCourses() -> [EnrolledCourses] {
+        do {
+            let request = NSFetchRequest<EnrolledCourses>(entityName: "EnrolledCourses")
+            return try context.fetch(request)
+        } catch {
+            print("Failed to fetch enrolled courses: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func removeFromEnrolledCourses(id: String) {
+        if let enrolledCourse = getEnrolledCourses().first(where: { $0.courseId == id }) {
+            context.delete(enrolledCourse)
+            save()
+        }
+    }
+    
+    private func save() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                fatalError("CoreData save failed. \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func delete() {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "EnrolledCourses")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print("Failed to delete all users: \(error)")
+        }
+    }
+}
